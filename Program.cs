4c1b7aacd140444c8f54ce374fd09e90a372ee10
Konstantin-AKV <abdulaev-kostya@mail.ru>
2022-06-11 00:40:26 +0300

    Загрузка ДЗ на гитхаб

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №1
+/* Console.WriteLine("Введите число № 1: ");
+int num_1 = int.Parse(Console.ReadLine ());
+Console.WriteLine ("Введите число № 2: ");
+int num_2 = int.Parse(Console.ReadLine ());
+if (num_1 == num_2)
+{
+     Console.WriteLine("Числа равны");
+}
+else if (num_1 > num_2)
+     {
+     Console.WriteLine(("MAX = " + num_1) + (";") + ("Min = " + num_2));
+     }
+     else Console.WriteLine(("MAX = " + num_2) + (";") + ("Min = " + num_1)); */
+
+
+//Задача № 2
+/* Console.WriteLine("Введите число № 1: ");
+int num_1 = int.Parse(Console.ReadLine ());
+Console.WriteLine ("Введите число № 2: ");
+int num_2 = int.Parse(Console.ReadLine ());
+Console.WriteLine ("Введите число № 3: ");
+int num_3 = int.Parse(Console.ReadLine ());
+int maxi = num_1;
+if (num_2 > maxi) maxi =num_2;
+if (num_3 > maxi) maxi =num_3;
+Console.WriteLine("max = " + maxi); */
+
+//Задача № 3
+/* Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine ());
+if (num % 2 == 0)
+{
+Console.WriteLine("Четное!");
+}
+else Console.WriteLine("Не четное!"); */
+
+//Задача № 4
+/* Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine ());
+int index = 0;
+while (index <= num)
+{
+     if (index % 2 == 0)
+          Console.Write(index + ",");
+     index ++;
+} */
